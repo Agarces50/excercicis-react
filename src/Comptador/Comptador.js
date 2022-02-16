e9567e9687a228962b12478e2777640ba654a000
LAB1INF-12\Administrador <agarcesb50@gmail.com>
2022-02-16 20:34:15 +0100

    galeria+cambios

---
+++ src/Comptador/Comptador.js
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function Comptador() {
+  return (
+    <>
+      <h1>COMPTADOR</h1>
+    </>
+  );
+}
+
+export default Comptador;
---
+++ src/Galeria/Galeria.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "../App.css";
+
+const imagenes = ["limonero.jpg", "manzano.jpg", "naranjo.jpg", "peral.jpg"];
+
+function Galeria() {
+  const [imagenOriginal, setImagenOriginal] = useState("expandido");
+
+  // nota :por defecto sale este sera el primero que vemos el original con su valor q se lo da el css
+
+  const [fotoClick, setFotoClick] = useState(null);
+
+  // nota : este es el valor por defecto que es 0 asi cuando hacemos clic cambia estado
+
+  const apertura = (fotoAbiertaApertura) => {
+    console.log(fotoAbiertaApertura);
+    setFotoClick(fotoAbiertaApertura);
+    imagenOriginal === "original"
+      ? setImagenOriginal("expandido")
+      : setImagenOriginal("original");
+  };
+
+  // nota:esto quiere decir que tenemos una variable que hemos llamado apertura , se podria llamar de cualquier otra
+  // manera, y tiene un parametro que lo llamo foto abierta que en el momento que abra la foto quedara almacenada en
+  // el estado pq por defecto era nullo asi que sera esta la ultima que quedara.
+  // Entonces decimos que si la imagen original es igual a la medida basica pues que ponga la otra medida que la he
+  // llamado expandida y si esta en expandida que ponga la original la medida pequeña.
+
+  // Nota: decimos que la variable imagenes,cada vez que cambie ponga una classe original
+  // en css dando un valor en este caso un tamaño. en el de debajo,le ponemos que en orígen
+  // el valor es nulo por eso al hacer clikc saldrá algo.
+
+  return (
+    <>
+      <div className="contenedor">
+        {imagenes.map((fotoAbierta) => (
+          <div key={fotoAbierta}>
+            <img
+              onClick={() => apertura(fotoAbierta)}
+              className={
+                fotoClick === fotoAbierta ? imagenOriginal : "original"
+
+                // nota: en este caso se hace para que cuando hagas onclik ,lo demás se quede peque,
+                // asi decimos que cuando haga click que no hay nada pq viene de un valor cero ,
+                // en el onlick hace el ternario y en la classe name dice que si la imagen es la expandida
+                // las demas sean originales.
+                // queremos decir que cuando hagamos click empezara expandido si ponemos original empezara
+                // pequeño.
+              }
+              src={`imagenes/${fotoAbierta}`}
+              alt={`imagenes/${fotoAbierta}`}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Galeria;
