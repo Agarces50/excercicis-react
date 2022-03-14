2aee426c3fdc0da5c6528495ad65c4112e3c40e1
LAB1INF-12\Administrador <agarcesb50@gmail.com>
2022-03-14 18:39:02 +0100

    detalle del pdf

--- src/pages/Activitat.js
+++ src/pages/Activitat.js
@@ -2,3 +2,4 @@
 // export default function Activitat() {
 //   return <Document file="/Imagenes/Arxivo.pdf">Arxivo</Document>;
 // }
+// hay que poner como el minion lo de public pq sino el browser no encuentra la ruta
